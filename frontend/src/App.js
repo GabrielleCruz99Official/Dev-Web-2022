aac7991ac447e7d25ef30cad63099fd044efe8b1
Gabrielle Cruz <gcruz99.official@gmail.com>
2022-04-16 17:01:18 +0200

    Reconstruction de repertoire Frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,20 @@
 import logo from './logo.svg';
+import React, { Fragment } from 'react';
 import './App.css';
-import { testcomp1 as T1, testcomp2 as T2 } from './components/testcomp1';
-import { Click } from './components/Click';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import FrontPage from './components/FrontPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Header here</h1>
-        <Click />
-      </header>
-    </div>
+    <BrowserRouter>
+      <Fragment>
+        <Navbar/>
+        <Routes>
+          <Route component={<FrontPage/>} path="/" />
+        </Routes>
+      </Fragment>
+    </BrowserRouter>
   );
 }
 
--- frontend/src/components/Navbar.js
+++ frontend/src/components/Navbar.js
@@ -1,17 +1,10 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
-
-export function Navbar(){
+function Navbar(){
     return(
-        <>
-            <div>
-                <li>
-                    <Link to="/">Home</Link>
-                </li>
-                <li>
-                    <Link to="/store">Home</Link>
-                </li>
-            </div>
-        </>
+       <>
+        <p>Test</p>
+       </> 
     )
 }
+
+export default Navbar;
