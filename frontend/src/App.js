3f55f6fe8e2561f6be40a5cbc3475b8ea7c60514
Gabrielle Cruz <gcruz99.official@gmail.com>
2022-04-17 19:59:57 +0200

    Ajoute des nouveaux fichiers frontend

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,15 +1,15 @@
 import logo from './logo.svg';
+import React from 'react';
 import './App.css';
-import { testcomp1 as T1, testcomp2 as T2 } from './components/testcomp1';
-import { Click } from './components/Click';
+import { Link } from 'react-router-dom';
+import Navbar from './components/Navbar'
+import FrontPage from './components/FrontPage';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Header here</h1>
-        <Click />
-      </header>
+    <div>
+      <Navbar />
+      <h1>Test</h1>
     </div>
   );
 }
---
+++ frontend/src/components/FrontPage.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+function FrontPage(){
+    return(
+        <div>
+            <p>Welcome to the Front Page!</p>
+        </div>
+    )
+}
+
+export default FrontPage;
