0928a697af375f20672debd0d9306c49b8e0d2db
Gabrielle Cruz <gcruz99.official@gmail.com>
2022-04-24 21:34:01 +0200

    Modification du page de commande

--- frontend/src/components/OrderPage.js
+++ frontend/src/components/OrderPage.js
@@ -3,8 +3,18 @@ import React from 'react';
 function OrderPage() {
     return(
         <div>
-
-        </div>)
+            <h1>Order Page</h1>
+            <div className="card">
+                <div className="card-header">
+                    Test
+                </div>
+                <div className="card-body">
+                    <h5 className="card-title">Basket</h5>
+                    <p>Stuff...</p>
+                </div>
+            </div>
+        </div>
+    )
 }
 
 export default OrderPage;
--- frontend/src/components/common/Navbar.js
+++ frontend/src/components/common/Navbar.js
@@ -1,23 +1,24 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
 import Logo from '../../visuals/logosensoria.png';
+import './Navbar.css';
 
 function Navbar(){
     return(
-        <nav className="navbar navbar-expand-lg navbar-light bg-light">
-            <div className="container">
-                <Link to="/">
-                    <img
-                        src={Logo}
-                        alt="Sensoria"
-                        title="Sensoria"
-                        height="50"
-                    />
-                </Link>
-                <Link to="/about">About Us</Link>
-                <Link to="/click">Click</Link>
-                <Link to="/store">Store</Link>
-            </div>
+        <nav className="link navbar navbar-expand-lg navbar-light bg-light">
+            <Link to="/">
+                <img
+                    src={Logo}
+                    alt="Sensoria"
+                    title="Sensoria"
+                    height="50"
+                />
+            </Link>
+            <Link to="/about">Notre Concept</Link>
+            <Link to="/store">S'abonner</Link>
+            <Link to="/">Contactez-nous</Link>
+            <Link to="/">Exemples du BOX</Link>
+            <Link to="/login">Espace Client</Link>
         </nav>
     )
 }
