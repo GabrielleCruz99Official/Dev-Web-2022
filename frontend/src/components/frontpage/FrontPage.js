f3656528ed5f1f2220b1c2dd505465eef59910c8
Gabrielle Cruz <gcruz99.official@gmail.com>
2022-05-01 16:12:06 +0200

    Ajoute de compteur de box du mois dans le page de garde - WIP
    Co-authored-by: Gabrielle Cruz <gcruz99.official@gmail.com>
    Co-authored-by: matthew everard <Mattheweverard85@gmail.com>

--- frontend/src/components/frontpage/FrontPage.js
+++ frontend/src/components/frontpage/FrontPage.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import Countdown from './Countdown';
+import CountdownTimer from './countdown/CountdownTimer';
 
 function FrontPage(){
     return(
@@ -18,7 +18,22 @@ function FrontPage(){
                     </p>
                 </div>
             </div>
-            <Countdown />
+            <div className="card">
+                <h5 className="card-title">Box du Mois</h5>
+                <CountdownTimer targetDate={new Date().getTime() + (3 * 24 * 60 * 60 * 1000)} />
+                <div className="card-body">
+                    <h6>Intro Pack</h6>
+                    <p>It is a long established fact that a reader will be distracted 
+                        by the readable content of a page when looking at its layout. 
+                        The point of using Lorem Ipsum is that it has a more-or-less normal distribution of letters,
+                        as opposed to using 'Content here, content here', making it look like readable English. 
+                        Many desktop publishing packages and web page editors now use Lorem Ipsum as their default model text, 
+                        and a search for 'lorem ipsum' will uncover many web sites still in their infancy. 
+                        Various versions have evolved over the years, sometimes by accident, 
+                        sometimes on purpose (injected humour and the like).
+                    </p>
+                </div>
+            </div>
         </div>
     )
 }
